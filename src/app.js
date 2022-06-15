9379f6e2cdbe50000666e6245951f5e65842e2f0
TonyDCoder5 <tonyaflex5@gmail.com>
2022-06-15 11:46:15 -0600

    redeploy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
@@ -8,6 +9,7 @@ const reviewsRouter = require("./reviews/reviews.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
