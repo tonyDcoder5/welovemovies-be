74c6e295cd81fdb4200027fd147bbacbb12f0e47
TonyDCoder5 <tonyaflex5@gmail.com>
2022-06-15 12:16:24 -0600

    redeploy

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -2,16 +2,18 @@ const knex = require("../db/connection");
 const reduceProperties = require("../utils/reduce-properties");
 
 const reduceMovies = reduceProperties("theater_id", {
+    "movie_id": ["movies", null, "movie_id"],
     "title": ["movies", null, "title"],
     "rating": ["movies", null, "rating"],
-    "runtime_in_minutes": ["movies", null, "runtime_in_minutes"]
+    "runtime_in_minutes": ["movies", null, "runtime_in_minutes"],
+    "image_url": ["movies", null, "image_url"]
   });
 
 function list() {
     return knex("theaters as t")
     .join("movies_theaters as mt", "mt.theater_id", "t.theater_id")
     .join("movies as m", "mt.movie_id", "m.movie_id")
-    .select("t.*", "m.title", "m.rating", "m.runtime_in_minutes")
+    .select("t.*", "m.title", "m.rating", "m.runtime_in_minutes", "m.image_url", "m.movie_id")
     .where("mt.is_showing", true)
     .andWhere(knex.raw("m.movie_id = mt.movie_id"))
     .andWhere(knex.raw("mt.theater_id = t.theater_id"))
